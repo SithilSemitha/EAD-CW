915381187e44f050e711e7e24495c67b388b02c7
SithilSemitha <sithilsemithad@gmail.com>
2025-01-08 18:52:49 +0530

    Designed the UI
    designed the User interface and connected the database

---
+++ EAD Cousrwork/CW 1/src/cw/pkg1/CW1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cw.pkg1;
+
+/**
+ *
+ * @author Amil De Silva
+ */
+public class CW1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        LoginFrame F1=new LoginFrame();
+        F1.setTitle("Log In");
+        F1.setVisible(true);
+       
+    }
+    
+}
